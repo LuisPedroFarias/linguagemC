f4fb1cbd7576b0148e63bdf3c2743b1066bc8c16
LuisPedroFarias <161033603+LuisPedroFarias@users.noreply.github.com>
2024-03-18 09:02:19 -0300

    Add files via upload

---
+++ nota_do_aluno (2).c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(){
+	
+	char nome [200] = "";
+	int  idade, soma;
+	float  media,primeiraNota, segundaNota, terceiraNota, quartaNota;
+	
+	//solicitando informações ao aluno
+	
+	printf("Digite seu nome: ");
+	scanf("%s", &nome);
+	printf("Digite sua idade: ");
+	scanf("%i", &idade);
+	printf("Digite a primeira nota: ");
+	scanf("%f", &primeiraNota);
+	printf("Digite a segunda nota: ");
+	scanf("%f", &segundaNota);
+	printf("Digite a terceira nota: ");
+	scanf("%f", &terceiraNota);
+	printf("Digite a quarta nota: ");
+	scanf("%f", &quartaNota);
+	
+	fflush(stdin);
+	
+	
+	//calculando média do aluno.
+	
+	media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+	
+	
+	//exibindo resultado para o aluno.
+	
+	
+	printf("\n==== EXIBINDO RESULTADO ====");
+	printf("\nNome: %s \n", nome);
+	printf("\nIdade: %i \n", idade);
+	printf("\nMédia: %4.f \n", media);
+	
+
+	getchar();
+	
+	return 0;
+	
+	
+}
