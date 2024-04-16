bf05d62cd4563804191fae355d90f071d22e43a4
LuisPedroFarias <161033603+LuisPedroFarias@users.noreply.github.com>
2024-04-16 11:21:50 -0300

    ordem_Inversa.c

--- Sem Título6.c
+++ Sem Título6.c
@@ -6,17 +6,24 @@
 
 
 int main(){
-	
-	int contador = 0;
-	int numero[TAM];
-	
-	for(contador = 0; contador < 5; contador++){
-		printf("Insira o %d numero", contador + 1);
-		scanf("%d", &numero[contador]);
+		setlocale(LC_ALL, ");
+	int numero[TAMANHO], somaPositivos = 0 ;
+	int i, quantidadeNegativos = 0;
+
+		printf("=== SOLICITANDO DADOS PARA O USÚARIO === \n");	
+	for(i = 0; i < TAMANHO; i++){
+	   do {
+		printf("Insira o %d numero", i + 1);
+		scanf("%d", &numero[i]);
+	}while (numero[i] < 0 || numeros[i] % 2 == 1);
+		
+	}
+	printf("\n=== EXIBINDO DADOS PARA O USÚARIO NA ORDEM INVERSA === \n");
+	for (i = 0; i < TAMANHO; i++){
+		printf("%iº número: %i \n", i+1, numeros[i]);
+
+		
 	}
 	
-	
-	
-	
-	
+return 0;	
 }
